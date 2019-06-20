70493b29971fb7aaec50999c5c4d7c77b0ee0eec
Max Jonathan Rodriguez B <maxjrb@openitsinaloa.com>
2019-06-19 21:00:16 -0600

    main.py modificado
    añadida linea de prueba de login

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Bottle()
 
 @app.route('/hola', method='GET')
 def get_list_games():
-    return "<h1>Hola Mundo</h1>"
+    return "<h1>Hola Mundo</h1><br><h2>Esto es una prueba de login</h2>"
+
 
 run(app, host='0.0.0.0', port=3000, debug=True)
