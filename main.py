65499eb3d2b58c5dc25a020ed4d4f0733efc15ef
Max Jonathan Rodriguez B <maxjrb@openitsinaloa.com>
2019-12-05 00:13:39 -0700

    Bumped to python 3.6.9 Added hello and hello_name methods added validation to run in dev or gunicorn Added requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from bottle import Bottle, run, request
-import sqlite3
 
 app = Bottle()
 
 @app.route('/hola', method='GET')
-def get_list_games():
-    return "<h1>Hola Mundo</h1><br><h2>Esto el login</h2>"
+@app.route('/', method='GET')
+def hello():
+    return "<h1>Hola Mundo</h1><br><h2></h2>"
 
-#comentario
-#otro comentario
-run(app, host='0.0.0.0', port=3000, debug=True)
+@app.get('/hola/<name>')
+def hello_name(name):
+    return f'<h1>Hola {name}</h1><br><h2></h2>'
+
+if __name__ == "__main__":
+    run(app, host='0.0.0.0', port=3000, debug=True)
+
